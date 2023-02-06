e93ad25e472a6ebafea437ca32ce4af2d75c04a8
Atticus6 <94210351+Atticus6@users.noreply.github.com>
2023-02-06 23:48:17 +0800

    改为在线在线

--- main.sh
+++ main.sh
@@ -1,1 +1,31 @@
+if [ -f "./halo.jar" ];then
+  echo "项目部署过，重新部署请删除halo.jar"
+  else
+    echo '6. halo-1.4'
+    echo '5. halo-1.5'
+    echo '4. halo-1.6'
+    echo '3. halo-2.0'
+    echo '2. halo-2.1'
+    echo '1. halo-2.2'
+    read -p "请输入你需要的版本号version（默认最新）" select
+    echo 'downloading...'
+    case $select in
+    6)
+        curl -L https://github.com/halo-dev/halo/releases/download/v1.4.17/halo-1.4.17.jar --output halo.jar
+        ;;
+    5)
+        curl -L https://github.com/halo-dev/halo/releases/download/v1.5.5/halo-1.5.5.jar --output halo.jar
+        ;;
+    4)
+        curl -L https://github.com/halo-dev/halo/releases/download/v1.6.0/halo-1.6.0.jar --output halo.jar
+        ;;
+    *)
+        echo 'error'
+        ;;
+
+
+
+    esac
+fi
+
 java -jar halo.jar
