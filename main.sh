f3d93eb6f82020f8350b1ba123450a9485946ebc
Atticus6 <94210351+Atticus6@users.noreply.github.com>
2023-02-07 14:09:52 +0800

    添加了多个版本

--- main.sh
+++ main.sh
@@ -1,24 +1,31 @@
 if [ -f "./halo.jar" ];then
   echo "项目部署过，重新部署请删除halo.jar"
   else
-    echo '6. halo-1.4'
-    echo '5. halo-1.5'
-    echo '4. halo-1.6'
-    echo '3. halo-2.0'
-    echo '2. halo-2.1'
-    echo '1. halo-2.2'
+    echo '1. halo-1.4'
+    echo '2. halo-1.5'
+    echo '3. halo-1.6'
+    echo '4. halo-2.0'
+    echo '5. halo-2.1'
+    echo '6. halo-2.2'
+    echo '7. halo-2.3'
     read -p "请输入你需要的版本号version（默认最新）" select
     echo 'downloading...'
     case $select in
-    6)
+    1)
         curl -L https://github.com/halo-dev/halo/releases/download/v1.4.17/halo-1.4.17.jar --output halo.jar
         ;;
-    5)
+    2)
         curl -L https://github.com/halo-dev/halo/releases/download/v1.5.5/halo-1.5.5.jar --output halo.jar
         ;;
-    4)
+    3)
         curl -L https://github.com/halo-dev/halo/releases/download/v1.6.0/halo-1.6.0.jar --output halo.jar
         ;;
+    4)
+        cp halo-2.0.0.jar halo.jar
+        ;;
+    7)
+       cp halo-2.3.0.jar halo.jar
+       ;;
     *)
         echo 'error'
         ;;
